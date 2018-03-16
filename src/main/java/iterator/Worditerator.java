d0f8f3fe56619fdb38678942dc83ff7ee32c0abd
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-03-16 20:11:19 +0100

    Worditerator: Create and fill class Wordditerator

---
+++ src/main/java/iterator/Worditerator.java
@@ -0,0 +1,34 @@
+package iterator;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class Worditerator implements Iterator{
+    List <String> words;
+    Integer index = 0;
+
+    public Worditerator(List <String> words){
+        this.words = words;
+    }
+
+    public boolean hasNext(){
+        boolean next = false;
+        if(index < words.size()){
+            next = true;
+        }
+
+        return next;
+    }
+
+    public String next(){
+        String word = null;
+        if(hasNext()){
+            word = words.get(index++);
+        }
+        return word;
+    }
+
+    public void remove(){
+        words.remove(index);
+    }
+}
