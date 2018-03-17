86c3a396a39dbbb50a09448dd05d0331163fe4b9
Leanka <comamydzisiajnasniadanie@gmail.com>
2018-03-18 00:08:31 +0100

    FileContent: Create and fill getFile() and validateFileCo ntent(); AdjustgetFileContent() and constructor to use those methods

--- src/main/java/textIterator/FileContent.java
+++ src/main/java/textIterator/FileContent.java
@@ -4,6 +4,7 @@ import iterator.Chariterator;
 import iterator.Worditerator;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Iterator;
@@ -18,16 +19,35 @@ public class FileContent implements IterableText{
     public FileContent(String filename) throws FileNotFoundException{
         this.filename = filename;
         this.fileContent = getFileContent();
+        validateFileContent();
     }
 
     private String getFileContent() throws FileNotFoundException{
-        String filePath = System.getProperty("user.dir") + "/src/main/resources/" + filename;
-        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+
+        File file = getFile();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
         return reader
                 .lines()
                 .map(String::toUpperCase)
                 .map(line -> line.replaceAll("[^A-Z ]", ""))
-                .collect(Collectors.joining(" "));
+                .collect(Collectors.joining(" ")).trim();
+    }
+    private File getFile() throws FileNotFoundException{
+        String filePath = System.getProperty("user.dir") + "/src/main/resources/" + filename;
+        File file = new File(filePath);
+
+        if(!file.isFile()){
+            throw new FileNotFoundException("File not found!");
+        }else if(file.length() == 0){
+            throw new FileNotFoundException("File found, but it's empty!");
+        }
+        return file;
+    }
+
+    private void validateFileContent() throws FileNotFoundException{
+        if(fileContent.trim().isEmpty()){
+            throw new FileNotFoundException("File is empty!");
+        }
     }
 
     public Iterator<String> chariterator() {
